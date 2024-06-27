a2e0a146cf1d6c1c8342ae12e923bc3faf3a4756
Redevil15 <brandonf2345@gmail.com>
2024-06-27 14:17:39 -0600

    chore: Update database configuration to use environment variables
    This commit updates the database configuration in the `data-source.ts` file to use environment variables for the database connection details. The `host`, `port`, `username`, `password`, and `database` properties are now set using the corresponding environment variables. This allows for more flexibility and security in managing the database credentials.

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,14 @@
 import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'ecommercedb',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [],
     synchronize: false,
     logging: false,
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSourceOptions)
+    TypeOrmModule.forRoot(dataSourceOptions),
   ],
   controllers: [AppController],
   providers: [AppService],
